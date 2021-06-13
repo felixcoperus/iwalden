fd611e740b45da0b4738a03964db2f145536c850
Felix Coperus <felix@devfruits.com>
2021-06-13 17:44:48 +0200

    undo kk hektrak

--- bot.py
+++ bot.py
@@ -44,10 +44,10 @@ async def on_message(message):
 
     # !say -------------------------------------------------------------------------------
     if message.content.startswith('!say '):
-        # kk hektrak
-        if message.author.name == 'Hektrak':
-            await message.channel.send('phi-males gaan mij niet vertellen wat ik moet zeggen kk hektrak tfoeee', reference=message.to_reference())
-            return
+        # # kk hektrak
+        # if message.author.name == 'Hektrak':
+        #     await message.channel.send('phi-males gaan mij niet vertellen wat ik moet zeggen kk hektrak tfoeee', reference=message.to_reference())
+        #     return
 
         # repeat given text without the command prefix
         text = message.content[5:]
